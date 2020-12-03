4f3e30a577059f39e98c9f3788e4c1ad054ccef2
Csaba Okrona <ochronus@ochronus.com>
2020-12-03 19:02:24 +0100

    Scala for day 3

--- scala/src/main/scala/Runner.scala
+++ scala/src/main/scala/Runner.scala
@@ -1,9 +1,10 @@
 package com.ochronus
 
-import year2020.{Day01, Day02}
+import year2020.{Day01, Day02, Day03}
 
 object Runner {
   def main(args: Array[String]) = {
+    /*
     val problem1 = new Day01()
     println("Day 1 part 1: " + problem1.part1())
     println("Day 1 part 2: " + problem1.part2())
@@ -11,5 +12,11 @@ object Runner {
     val problem2 = new Day02()
     println("Day 2 part 1: " + problem2.part1())
     println("Day 2 part 2: " + problem2.part2())
+
+
+     */
+    val problem3 = new Day03()
+    println("Day 3 part 1: " + problem3.part1())
+    println("Day 3 part 2: " + problem3.part2())
   }
 }
---
+++ scala/src/main/scala/year2020/Day03.scala
@@ -0,0 +1,22 @@
+package com.ochronus.year2020
+
+import com.ochronus.utils.File
+
+class Day03 {
+  private val lines = File.readFileLines("day03.input.txt")
+  private val (width, height) = (lines.head.size, lines.size)
+  private val travelPatterns = Seq((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+
+  def part1(): Long = {
+    travelAndCount(3, 1)
+  }
+
+  def part2(): Long = {
+    travelPatterns.map(pattern => travelAndCount(pattern._1, pattern._2)).product
+  }
+
+  def travelAndCount(right: Int, down: Int): Long = {
+    val stepsDown = (height.toFloat / down).ceil.toInt
+    (0 until stepsDown).count(i => lines(i * down)((right * i) % width) == '#').toLong
+  }
+}
